37038f7a78a01894880eb11a41e08620c0711832
Nicolas Piganeau <nicolas.piganeau@ndp-systemes.fr>
2016-06-16 08:55:26 +0200

    [FIX] Removed yep-addons import in standard config  So as not to have branches issues between yep and yep-addons

--- config/config.go
+++ config/config.go
@@ -14,9 +14,9 @@
 
 package config
 
-// Import here all yep addons you need in your application
+// Import here anonymously all yep addons you need in your application
 import (
-	_ "github.com/npiganeau/yep-addons/web"
+//_ "github.com/npiganeau/yep-addons/web"
 )
 
 const (
