bc24daa4f72eb7c5025be84ea433e82c8d3ba1ec
Nicolas Piganeau <nicolas.piganeau@ndp-systemes.fr>
2017-01-28 14:09:30 +0100

    [FIX] Another missing import

--- yep/tests/t20_domains_test.go
+++ yep/tests/t20_domains_test.go
@@ -20,6 +20,7 @@ import (
 	"github.com/npiganeau/yep/pool"
 	"github.com/npiganeau/yep/yep/models"
 	"github.com/npiganeau/yep/yep/models/security"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestDomains(t *testing.T) {
